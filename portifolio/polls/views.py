ba490d2411f02b7670053fd3fa7959b245700787
Danilo Andrade <dandrade0803@gmail.com>
2023-08-06 18:02:11 -0300

    Django Tutorial - Escrevendo sua primeira aplicação Django, parte 4

--- portifolio/polls/views.py
+++ portifolio/polls/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Olá, mundo! Você está na view 'index' do app polls.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = f"Você está vendo o resultado da enquete {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Você está votando na enquete {question_id}.")
